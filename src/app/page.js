b23dd283011daf51f674c356d5c6e045ba3412ac
Darrion Whitfield <dwhitfield@ramfield.net>
2025-03-01 11:46:26 -0600

    added a bullet

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
       <section className="w-full max-w-5xl mt-8 bg-gray-800 shadow-md p-6 rounded-lg">
         <h2 className="text-2xl font-semibold text-purple-300">Our Services</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-          <div className="bg-purple-700 p-4 rounded-lg text-center">
+          <div className="bg-red-700 p-4 rounded-lg text-center">
             <h3 className="font-semibold text-xl text-gray-100">Custom Software</h3>
             <p className="text-gray-300 mt-2">Tailored software development for your needs.</p>
           </div>
